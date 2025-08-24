7fae994b378770095435476ddab07debffda4d15
Mihai Birsan <alexandru.m.birsan@gmail.com>
2025-08-24 19:25:03 +0000

    fixup: Reverted changes to project names — will not be parsed as Markdown

--- lib/ui/components/reports/TogglReportProjectList.svelte
+++ lib/ui/components/reports/TogglReportProjectList.svelte
@@ -2,7 +2,6 @@
   import { CurrentTimer } from "lib/stores/currentTimer";
   import { DailySummary } from "lib/stores/dailySummary";
   import { secondsToTimeString } from "lib/util/millisecondsToTimeString";
-  import { renderMarkdown } from "lib/util/renderMarkdown";
   import { settingsStore } from "lib/util/stores";
 
   export let duration_seconds: number;
@@ -39,11 +38,7 @@
       <div class="is-flex is-align-items-center">
         <span class="project-circle" style="background-color: {e.color};" />
         <span class="ml-3 project-row-name" style="color: {e.color};">
-          {#if $settingsStore.parseMarkdown}
-            {@html e.name ? renderMarkdown(e.name) : "(No project)"}
-          {:else}
-            {e.name ? e.name : "(No project)"}
-          {/if}
+          {e.name ? e.name : "(No project)"}
         </span>
       </div>
       <span class="project-row-duration">{e.duration}</span>
