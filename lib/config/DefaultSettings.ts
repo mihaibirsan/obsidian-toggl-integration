714b39321aecb935548dcfb8825c92337d9501e9
Mihai Birsan <alexandru.m.birsan@gmail.com>
2025-08-24 20:23:31 +0000

    fixup: Fixed lint warning: Expected object keys to be in ascending order.

--- lib/config/DefaultSettings.ts
+++ lib/config/DefaultSettings.ts
@@ -3,11 +3,11 @@ import type { PluginSettings } from "./PluginSettings";
 export const DEFAULT_SETTINGS: PluginSettings = {
   apiToken: null,
   charLimitStatusBar: 40,
+  parseMarkdown: false,
   statusBarFormat: "m [minute]",
   statusBarNoEntryMesssage: "-",
   statusBarPrefix: "Timer: ",
   statusBarShowProject: false,
-  parseMarkdown: false,
   updateInRealTime: true,
   workspace: { id: "none", name: "None selected" },
 };
